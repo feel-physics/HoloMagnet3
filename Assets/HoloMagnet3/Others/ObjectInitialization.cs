ef50d258e1bd4d5bceaf918aef290740c2b27272
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-04-14 14:50:41 +0900

    Fix orientation of bar magnet

--- Assets/HoloMagnet3/Others/ObjectInitialization.cs
+++ Assets/HoloMagnet3/Others/ObjectInitialization.cs
@@ -8,11 +8,13 @@ public class ObjectInitialization : MonoBehaviour {
 
         if (gameObject.name == "PointMagnet01")
         {
+            /*  Todo: どのオブジェクトかを調べる 
             // 磁石を横向きに生成する
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
 
             transform.localPosition = new Vector3(0, 0, 0.7f);
             Debug.Log(gameObject.name + logMessage);
+            */
         }
         else if(gameObject.name == "Axises")
         {
@@ -21,8 +23,9 @@ public class ObjectInitialization : MonoBehaviour {
         }
         else if(gameObject.name == "BarMagnet01")
         {
-            transform.localPosition = new Vector3(0, 0f, 0f);
-            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.localPosition = new Vector3(0, 0f, 2f);
+            //transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
             Debug.Log(gameObject.name + logMessage);
         }
     }
