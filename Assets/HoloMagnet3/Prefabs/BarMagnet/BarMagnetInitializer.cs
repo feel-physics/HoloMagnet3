e6073f957abc5fa26834759346c5262912d52fcb
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-04-20 12:07:29 +0900

    Replace intance reference with object reference

--- Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetInitializer.cs
+++ Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetInitializer.cs
@@ -4,8 +4,10 @@ public class BarMagnetInitializer : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-        BarMagnetModel.Instance.NorthPoleReference = transform.Find("North Body/North Pole").gameObject;
-        BarMagnetModel.Instance.SouthPoleReference = transform.Find("South Body/South Pole").gameObject;
-        BarMagnetModel.Instance.MagneticForceLineReference = transform.Find("MagneticForceLine").gameObject;
+        gameObject.GetComponent<BarMagnetModel>().NorthPoleReference =
+            transform.Find("North Body/North Pole").gameObject;
+        //BarMagnetModel.Instance.NorthPoleReference = transform.Find("North Body/North Pole").gameObject;
+        //BarMagnetModel.Instance.SouthPoleReference = transform.Find("South Body/South Pole").gameObject;
+        //BarMagnetModel.Instance.MagneticForceLineReference = transform.Find("MagneticForceLine").gameObject;
     }
 }
