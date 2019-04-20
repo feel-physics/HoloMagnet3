85edc98b6b5b4bb502aabbae873eccd56c7b1b37
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-04-20 12:13:37 +0900

    Update BarMagnetInitializer.cs

--- Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetInitializer.cs
+++ Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetInitializer.cs
@@ -6,6 +6,10 @@ public class BarMagnetInitializer : MonoBehaviour {
 	void Awake () {
         gameObject.GetComponent<BarMagnetModel>().NorthPoleReference =
             transform.Find("North Body/North Pole").gameObject;
+        gameObject.GetComponent<BarMagnetModel>().SouthPoleReference =
+            transform.Find("South Body/South Pole").gameObject;
+        gameObject.GetComponent<BarMagnetModel>().MagneticForceLineReference = 
+            transform.Find("MagneticForceLine").gameObject;
         //BarMagnetModel.Instance.NorthPoleReference = transform.Find("North Body/North Pole").gameObject;
         //BarMagnetModel.Instance.SouthPoleReference = transform.Find("South Body/South Pole").gameObject;
         //BarMagnetModel.Instance.MagneticForceLineReference = transform.Find("MagneticForceLine").gameObject;
