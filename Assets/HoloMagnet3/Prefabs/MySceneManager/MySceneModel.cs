18dff0aa9bade3f902dd5ab0bcfb50159800238a
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-04-26 15:55:40 +0900

    Create MySceneModel.cs

---
+++ Assets/HoloMagnet3/Prefabs/MySceneManager/MySceneModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MySceneModel : MonoBehaviour {
+
+    // シーンのリストをenumで作る
+    public enum MyScene { Introduction, Compass_One, Compasses_2D, Compasses_3D }
+    public MyScene scene;
+
+    // シーン名とenumのシーンとを対応させる
+    Dictionary<string, MyScene> sceneDic = new Dictionary<string, MyScene>() {
+        {"Introduction",    MyScene.Introduction },
+        {"Compass_One",     MyScene.Compass_One },
+        {"Compasses_2D",    MyScene.Compasses_2D },
+        {"Compasses_3D",    MyScene.Compasses_3D }
+    };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
