e55cbcc2587a0658c977febf73b0b19c30cb4738
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-04-28 02:28:47 +0900

    お掃除

--- Assets/HoloMagnet3/Prefabs/CompassesManager/CompassesCreator.cs
+++ Assets/HoloMagnet3/Prefabs/CompassesManager/CompassesCreator.cs
@@ -2,22 +2,13 @@
 
 public class CompassesCreator : MonoBehaviour
 {
-    Vector3 shiftVector =
-    new Vector3(0, 0, 2);
+    Vector3 shiftVector = new Vector3(0, 0, 2);
 
     float pitchCompass = 0.03f;
-    /*
-    int numCompassX = 8;
-    int numCompassY = 8;
-    int numCompassZ = 6;
 
-    int numCompassX = 8;
-    int numCompassY = 8;
-    int numCompassZ = 1;
-    */
-    private int numCompassX = 8;
-    private int numCompassY = 8;
-    private int numCompassZ = 6;
+    private int numCompassX = 1;
+    private int numCompassY = 1;
+    private int numCompassZ = 1;
 
     void Start()
     {
@@ -57,7 +48,7 @@ public class CompassesCreator : MonoBehaviour
                     new Vector3(
                         pitchCompass * w - (numCompassX - 1.0f) / 2.0f * pitchCompass,  // x軸に対し対称に±方向に方位磁針を並べる
                         pitchCompass * h - (numCompassY - 1.0f) / 2.0f * pitchCompass,  // y軸に対し対称に±方向に方位磁針を並べる
-                        pitchCompass * d - (numCompassZ - 1.0f) / 2.0f * pitchCompass) // z軸方向に方位磁針を並べる
+                        pitchCompass * d - (numCompassZ - 1.0f) / 2.0f * pitchCompass)  // z軸に対し対称に±方向に方位磁針を並べる
                         + shiftVector;
 
                     var compassCloned = Instantiate(compass,
