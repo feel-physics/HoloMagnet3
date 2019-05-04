8914567b6120e070803259886a624b940273083e
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-05-04 16:51:02 +0900

    3次元の場合は開始時に棒磁石が音を鳴らしながら単振動している：音のみ

---
+++ Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetAutoMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarMagnetAutoMover : MonoBehaviour {
+
+    public AudioClip ACMoving;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        if (MySceneManager.Instance.MyScene == MySceneManager.MySceneEnum.Compasses_3D)
+        {
+            MoveAutomatically();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void MoveAutomatically()
+    {
+        audioSource = GetComponents<AudioSource>()[0];
+        audioSource.clip = ACMoving;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
