a21671018c55268b018bdf54d7702a37fc510d99
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-05-04 18:00:01 +0900

    次元の場合は開始時に棒磁石が音を鳴らしながら単振動している：移動

--- Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetAutoMover.cs
+++ Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetAutoMover.cs
@@ -1,30 +1,49 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarMagnetAutoMover : MonoBehaviour {
+public class BarMagnetAutoMover : Singleton<BarMagnetAutoMover> {
 
     public AudioClip ACMoving;
     AudioSource audioSource;
+    public bool IsMoving = false;
+
+    private Vector3 initialPosition;
+    private Vector3 displacePosition;
 
     // Use this for initialization
     void Start () {
+        // 3次元のシーンのみ作動する
         if (MySceneManager.Instance.MyScene == MySceneManager.MySceneEnum.Compasses_3D)
         {
-            MoveAutomatically();
+            IsMoving = true;
+
+            // 初期化
+            initialPosition = gameObject.transform.position;
+            displacePosition = new Vector3(0, 0, 0);
+
+            // 音を鳴らす
+            audioSource = GetComponents<AudioSource>()[0];
+            audioSource.clip = ACMoving;
+            audioSource.loop = true;
+            audioSource.Play();
         }
     }
 
     // Update is called once per frame
     void Update () {
-		
+		if (IsMoving)
+        {
+            MoveAutomatically();
+        }
 	}
 
     void MoveAutomatically()
     {
-        audioSource = GetComponents<AudioSource>()[0];
-        audioSource.clip = ACMoving;
-        audioSource.loop = true;
-        audioSource.Play();
+        // 動かす
+        displacePosition.x = 0.3f * Mathf.Sin(Time.time / 3f);
+        gameObject.transform.position =
+            initialPosition + displacePosition;
     }
 }
