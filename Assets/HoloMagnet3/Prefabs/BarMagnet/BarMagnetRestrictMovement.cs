f8601c4ada964a5fbd5b72e1081bb5a80f0be09c
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-05-11 14:34:01 +0900

    2次元のシーンのときはz座標を固定する

---
+++ Assets/HoloMagnet3/Prefabs/BarMagnet/BarMagnetRestrictMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarMagnetRestrictMovement : MonoBehaviour {
+
+    public bool Is2D = false;
+
+	// Use this for initialization
+	void Start () {
+        if (MySceneManager.Instance.MyScene == MySceneManager.MySceneEnum.Compasses_2D)
+        {
+            Is2D = true;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Is2D) return;  // 2次元のシーンでなければreturn  Todo: 必要か？
+        Vector3 pos = transform.position;
+        pos.z = 2;
+        transform.position = pos;
+    }
+}
