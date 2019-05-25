4fac4701724b8c143b37b8a518665fb63dd0955a
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2019-05-25 14:08:57 +0900

    Calculator：関連クラスの修正

--- Assets/HoloMagnet3/Resources/Compass180509/CompassManagedlyUpdater.cs
+++ Assets/HoloMagnet3/Resources/Compass180509/CompassManagedlyUpdater.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 
 public class CompassManagedlyUpdater : MonoBehaviour
 {
-    GameObject[] southPoles;
-    GameObject[] northPoles;
+    //GameObject[] southPoles;
+    //GameObject[] northPoles;
+
+    Transform[] southPolesTransform;
+    Transform[] northPolesTransform;
 
     void Start()
     {
-        northPoles = GameObject.FindGameObjectsWithTag("North Pole");
-        southPoles = GameObject.FindGameObjectsWithTag("South Pole");
+        //northPoles = GameObject.FindGameObjectsWithTag("North Pole");
+        //southPoles = GameObject.FindGameObjectsWithTag("South Pole");
+
+        southPolesTransform = GameObject.FindGameObjectsWithTag("North Pole").
+            Select(go => go.transform).
+            ToArray();
+        northPolesTransform = GameObject.FindGameObjectsWithTag("South Pole").
+            Select(go => go.transform).
+            ToArray();
+
     }
 
     public void ManagedlyUpdate()
@@ -17,14 +28,24 @@ public class CompassManagedlyUpdater : MonoBehaviour
         // コンパスを回転させ、明るさを変える
         Rotate();
     }
+
+    private static Pole[] ToPoleArray(Transform[] transforms)
+    {
+        var poles = new Pole[transforms.Length];
+        for (var index = 0; index < poles.Length; index++)
+        {
+            poles[index] = new Pole { position = transforms[index].position };
+        }
+        return poles;
+    }
     
     void Rotate()
     {
         // 合力ベクトル
         Vector3 forceResultant = 
             MagneticForceCalculator.Instance.ForceResultant(
-                northPoles.Select(pole => new Pole { position = pole.transform.position }).ToArray(),
-                southPoles.Select(pole => new Pole { position = pole.transform.position }).ToArray(),
+                ToPoleArray(southPolesTransform),
+                ToPoleArray(northPolesTransform),
                 transform.position);
 
         // コンパスの向きを設定する
