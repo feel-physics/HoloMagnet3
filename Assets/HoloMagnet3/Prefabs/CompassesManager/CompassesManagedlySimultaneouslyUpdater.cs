39c38792b7b4c61aa2744d032b015e0701b5ed2a
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2019-07-08 14:47:20 +0900

    二つ目のマグネットが無くても例外が出ないように

--- Assets/HoloMagnet3/Prefabs/CompassesManager/CompassesManagedlySimultaneouslyUpdater.cs
+++ Assets/HoloMagnet3/Prefabs/CompassesManager/CompassesManagedlySimultaneouslyUpdater.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 // Todo: 後でクラス名をRenameする
 public class CompassesManagedlySimultaneouslyUpdater : MonoBehaviour
 {
+    [SerializeField]
     private BarMagnetModel magnet;  // Todo: 後でbarMagnet01Model に変える
-    // Todo: 後でbarMagnet01.northPoleになるようにする（松井さん？）
+                                    // Todo: 後でbarMagnet01.northPoleになるようにする（松井さん？）
+
+    [SerializeField]
     private GameObject barMagnet01NorthPole;
+    [SerializeField]
     private GameObject barMagnet01SouthPole;
 
+    [SerializeField]
     private GameObject barMagnet02NorthPole;
+    [SerializeField]
     private GameObject barMagnet02SouthPole;
+
     // Todo: 後で確認する Unity - Manual: Debugging DirectX 11/12 shaders with Visual Studio https://docs.unity3d.com/Manual/SL-DebuggingD3D11ShadersWithVS.html
     // Todo: 後でCompassesManagedlySimultaneouslyUpdaterをCompassesManagedlyUpdaterにRename
     // Todo: 後でBarMagnetMagneticForceLinesSimultaneouslyDrawerからSimultaneouslyを削除
     // Todo: 読み上げるテキストを作る
     // Todo: テキストを読み上げてくれるサービスを探す
     // Todo: コメントを英訳するかは相談する
+    // Todo: Storeに上げる準備（アイコンなど）をする（うまくいかないときはHoloMagnet3.oldをそのまま持ってくる）
+    // Todo: Storeに上げる
     // Todo: Storeの画像を変える
     // Todo: 後で動的に二次元シーン開始時にBarMagnetRistrictMovementをアタッチする
 
@@ -42,7 +51,7 @@ public class CompassesManagedlySimultaneouslyUpdater : MonoBehaviour
     void Update()
     {
         // Observer
-        var compasses = 
+        var compasses =
             CompassesModel.Instance.CompassesReferenceForManagedlyUpdate;
 
         if (compasses.Count > 0)  // Todo: IntroductionではCompassesManagerを消す
@@ -61,7 +70,7 @@ public class CompassesManagedlySimultaneouslyUpdater : MonoBehaviour
         int check = 1;
 
         //磁石とコンパスの位置差分を取得
-        var offset = magnet.transform.position - 
+        var offset = magnet.transform.position -
             CompassesModel.Instance.ParentTransform.position;
         //コンパスの間隔を取得
         var pitch = CompassesModel.Instance.pitch;
@@ -109,13 +118,29 @@ public class CompassesManagedlySimultaneouslyUpdater : MonoBehaviour
     void SetupForChangingBrightness()
     {
         magnet = FindObjectOfType<BarMagnetModel>();
-        GameObject barMagnet01 = GameObject.Find("BarMagnet01");
-        barMagnet01NorthPole = barMagnet01.transform.Find("North Body/North Pole").gameObject;
-        barMagnet01SouthPole = barMagnet01.transform.Find("South Body/South Pole").gameObject;
 
-        GameObject barMagnet02 = GameObject.Find("BarMagnet02");
-        barMagnet02NorthPole = barMagnet02.transform.Find("North Body/North Pole").gameObject;
-        barMagnet02SouthPole = barMagnet02.transform.Find("South Body/South Pole").gameObject;
+        try
+        {
+            GameObject barMagnet01 = GameObject.Find("BarMagnet01");
+            barMagnet01NorthPole = barMagnet01.transform.Find("North Body/North Pole").gameObject;
+            barMagnet01SouthPole = barMagnet01.transform.Find("South Body/South Pole").gameObject;
+        }
+        catch
+        {
+            Debug.Log("Could not find BarMagnet01.");
+        }
+
+        try
+        {
+            GameObject barMagnet02 = GameObject.Find("BarMagnet02");
+            barMagnet02NorthPole = barMagnet02.transform.Find("North Body/North Pole").gameObject;
+            barMagnet02SouthPole = barMagnet02.transform.Find("South Body/South Pole").gameObject;
+        }
+        catch
+        {
+            Debug.Log("Could not find BarMagnet02.");
+        }
+
 
         // 3次元の場合は3次元用の明るさの係数を使う
         if (MySceneManager.Instance.MyScene == MySceneManager.MySceneEnum.Compasses_3D)
@@ -144,22 +169,33 @@ public class CompassesManagedlySimultaneouslyUpdater : MonoBehaviour
         //var p = magnet.transform.position;
         var np1 = barMagnet01NorthPole.transform.position;
         var sp1 = barMagnet01SouthPole.transform.position;
-        var np2 = barMagnet02NorthPole.transform.position;
-        var sp2 = barMagnet02SouthPole.transform.position;
         var n1v4 = new Vector4(np1.x, np1.y, np1.z, 0);  // Convert to Vector4
         var s1v4 = new Vector4(sp1.x, sp1.y, sp1.z, 0);  // Convert to Vector4
-        var n2v4 = new Vector4(np2.x, np2.y, np2.z, 0);  // Convert to Vector4
-        var s2v4 = new Vector4(sp2.x, sp2.y, sp2.z, 0);  // Convert to Vector4
 
         // Set coordinates to Shader of Material of NORTH side of compass
+
         CompassesModel.Instance.MatNorth.SetVector("_NorthPole1Pos", n1v4);
         CompassesModel.Instance.MatNorth.SetVector("_SouthPole1Pos", s1v4);
-        CompassesModel.Instance.MatNorth.SetVector("_NorthPole2Pos", n2v4);
-        CompassesModel.Instance.MatNorth.SetVector("_SouthPole2Pos", s2v4);
+
         // Set coordinates to Shader of Material of SOUTH side of compass
         CompassesModel.Instance.MatSouth.SetVector("_NorthPole1Pos", n1v4);
         CompassesModel.Instance.MatSouth.SetVector("_SouthPole1Pos", s1v4);
-        CompassesModel.Instance.MatSouth.SetVector("_NorthPole2Pos", n2v4);
-        CompassesModel.Instance.MatSouth.SetVector("_SouthPole2Pos", s2v4);
+
+
+        if (barMagnet02NorthPole != null)
+        {
+
+            var np2 = barMagnet02NorthPole.transform.position;
+            var sp2 = barMagnet02SouthPole.transform.position;
+
+            var n2v4 = new Vector4(np2.x, np2.y, np2.z, 0);  // Convert to Vector4
+            var s2v4 = new Vector4(sp2.x, sp2.y, sp2.z, 0);  // Convert to Vector4
+
+            // Set coordinates to Shader of Material of NORTH side of compass
+            CompassesModel.Instance.MatNorth.SetVector("_NorthPole2Pos", n2v4);
+            CompassesModel.Instance.MatNorth.SetVector("_SouthPole2Pos", s2v4);
+            CompassesModel.Instance.MatSouth.SetVector("_NorthPole2Pos", n2v4);
+            CompassesModel.Instance.MatSouth.SetVector("_SouthPole2Pos", s2v4);
+        }
     }
 }
