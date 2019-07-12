5ac1c045ca7bb35359a9abff8cfd659da6af34e5
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2019-07-12 13:06:46 +0900

    #28 Aruco marker detection

---
+++ Assets/HoloMagnet3/Scripts/HoloLensCamera/AttachToCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Transformをカメラの子要素にする
+/// </summary>
+public class AttachToCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.parent = Camera.main.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localEulerAngles = Vector3.zero;
+
+
+    }
+
+}
