8deb128b30745ad9b0526f58b37d3be7f3213365
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2019-07-12 13:02:50 +0900

    #28 Add aruco package
    https://github.com/KeyMaster-/HoloLensArucoTracking/

---
+++ Assets/Aruco/Scripts/BringOffset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BringOffset : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (transform.hasChanged && target != null)
+        {
+            target.SetPositionAndRotation(transform.position, transform.rotation);
+        }
+	}
+}
