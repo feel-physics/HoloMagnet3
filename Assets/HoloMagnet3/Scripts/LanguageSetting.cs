4fca7fbe914242417e1b7b67f0c079390d60082b
Naoaki-Y <diberidarshi@yahoo.co.jp>
2020-08-19 07:49:32 +0900

    現在の言語設定情報の保持するための変数等を追加

--- Assets/HoloMagnet3/Scripts/LanguageSetting.cs
+++ Assets/HoloMagnet3/Scripts/LanguageSetting.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class LanguageSetting : MonoBehaviour
 {
+	//言語タイプ.
+	public enum LanguageType{
+		//日本語.
+		Japanese,
+		//英語.
+		English,
+	}
+
+	//アプリに設定された現在の言語設定.
+	private static LanguageType currentLanguageType = LanguageType.Japanese;
+	public static LanguageType CurrentLanguageType
+	{
+		get
+		{
+			return currentLanguageType;
+		}
+		set
+		{
+			currentLanguageType = value;
+		}
+	}
     // Start is called before the first frame update
     void Start()
     {
