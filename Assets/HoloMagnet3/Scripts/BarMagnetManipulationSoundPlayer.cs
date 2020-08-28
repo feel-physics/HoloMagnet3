890f0f0cbc16fa80b8f8b38336f38f479be3ff64
Naoaki-Y <diberidarshi@yahoo.co.jp>
2020-08-28 11:09:01 +0900

    棒磁石をつかんだ際に関連する音を再生/管理するための変数と初期化処理を追加

--- Assets/HoloMagnet3/Scripts/BarMagnetManipulationSoundPlayer.cs
+++ Assets/HoloMagnet3/Scripts/BarMagnetManipulationSoundPlayer.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class BarMagnetManipulationSoundPlayer : MonoBehaviour
 {
+	[SerializeField]
+	private AudioClip holdStartSound = null;
+	[SerializeField]
+	private AudioClip holdEndSound = null;
+	[SerializeField]
+	private AudioClip holdingSound = null;
+
+	[SerializeField]
+	private AudioSource audioSource = null;
+
+	// Start is called before the first frame update
+	void Start( )
+	{
+		if (audioSource == null)
+		{
+			audioSource = GetComponent<AudioSource>();
+		}
+		if (audioSource != null && holdingSound != null)
+		{
+			audioSource.clip = holdingSound;
+			audioSource.loop = true;
+		}
+	}
+
+/*	// Update is called once per frame
+	void Update( )
+	{
+
+	}*/
 
 }
